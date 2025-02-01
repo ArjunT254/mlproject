206cdf4a769843831113fdaf253790f4d348dc43
Arjun Thonoor <arjunt426@gmail.com>
2025-01-31 20:51:14 -0600

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -23,5 +23,11 @@ class CustomException(Exception):  # This is inheriting the parent exception
         
 
 
-
+# if __name__=="__main__":
+     
+#     try:
+#           a=1/0
+#     except Exception as e:
+#          logging.info("Divide by zero")
+#          raise CustomException(e,sys)
         
